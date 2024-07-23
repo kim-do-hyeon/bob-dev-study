904709d044927dc6bae56d4646a63eee9b2fd716
kim-do-hyeon <pental@kakao.com>
2024-07-23 22:51:16 +0900

    Add level 2 - 땅따먹기

---
+++ dohyeom/Lv2_땅따먹기.py
@@ -0,0 +1,7 @@
+def solution(land):
+    answer = 0
+    for i in range(1,len(land)):
+        for j in range(len(land[0])):
+            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
+    answer = max(land[len(land)-1])
+    return answer
