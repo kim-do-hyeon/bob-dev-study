b8dc4f6809bf6b4ad56077227ec2c749b1c0cac9
soyoung <rmfnqldk02@naver.com>
2024-07-31 10:51:48 +0900

    Add level 1 - 콜라츠 추측

---
+++ soyoung/Lv1_콜라츠 추측.py
@@ -0,0 +1,14 @@
+def solution(num):
+    answer = 0;
+    while num != 1:
+        if answer == 500:
+            return -1
+        elif num % 2 == 0:
+            num = num/2
+            answer = answer + 1
+        elif num%2 == 1:
+            num = num * 3 + 1
+            answer = answer + 1
+    return answer;
+    
+    return answer
